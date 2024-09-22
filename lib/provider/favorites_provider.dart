0a03e5e16d724e781004fa8b7fdf54f6079964b0
ROSHAN KUMAR YADAV <roshan099999@gmail.com>
2024-09-22 12:21:29 +0530

    adding new provider StateNotifier for make add or remove new element

---
+++ lib/provider/favorites_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mealsapp/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  // as in provider the value of any things like list can't be add or remove
+  // directly or edit the existing list or value  but we can assign new value always .
+  //  so we need to add or remove smartly
+
+  void toggleMealsFavoritesStatus(Meal meal) {
+    final mealIsFavorites = state.contains(meal);
+    if (mealIsFavorites) {
+      state = state.where((newMeal) => newMeal.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+final FavoriteMealsProvider = 
+StateNotifierProvider<FavoriteMealsNotifier,List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
